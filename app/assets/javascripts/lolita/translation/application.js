e1e270c8aa642e6107427a607946711786aff2bd
Arturs Meisters <arturs@ithouse.lv>
2012-04-18 18:55:01 +0300

    js changed to work with jQuery 1.7.x

--- app/assets/javascripts/lolita/translation/application.js
+++ app/assets/javascripts/lolita/translation/application.js
@@ -2,8 +2,8 @@ $(function(){
   $(".tab-language-switch li").live("click",function(){
     $(".tab-language-switch li").removeClass("active")
     $(this).addClass("active");
-    var locale = $(this).attr("data-locale")
-    var tab_name = $(this).attr("data-tab")
+    var locale = $(this).data("locale")
+    var tab_name = $(this).data("tab")
     $(".tab-content .language-wrap").hide(0)
     $("#"+locale+"_attributes_for_"+tab_name).show(0)
     resize_all_tinymce_editors()
