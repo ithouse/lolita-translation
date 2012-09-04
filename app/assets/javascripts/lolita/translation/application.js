d30f0cfa5270f55e1534e26eec2bbca4e08ea87d
Arturs Meisters <arturs@ithouse.lv>
2012-09-04 17:59:37 +0300

    language switch script updated to work in scopes

--- app/assets/javascripts/lolita/translation/application.js
+++ app/assets/javascripts/lolita/translation/application.js
@@ -1,12 +1,13 @@
 $(function(){
   $(".tab-language-switch li").live("click",function(){
-    $(".tab-language-switch li").removeClass("active");
+    $(this).parent().children("li").removeClass("active");
+    //$(".tab-language-switch li").removeClass("active");
     $(this).addClass("active");
-    var locale = $(this).data("locale");
-    var tab_name = $(this).data("tab");
-    var container = $(this).data("container") || ".tab-content";
-    $(container + " .language-wrap").hide(0)
-    $("#"+locale+"_for_"+tab_name).show(0)
-    resize_all_tinymce_editors()
-  })
-})
+    var locale = $(this).data("locale"),
+        tab_name = $(this).data("tab"),
+        container = $(this).data("container") || ".tab-content";
+    $(container + " .language-wrap").hide(0);
+    $("#"+locale+"_for_"+tab_name).show(0);
+    resize_all_tinymce_editors();
+  });
+});
